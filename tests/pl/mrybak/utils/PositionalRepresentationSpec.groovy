f3e9d01d8222c58875a99654b82a12128f8d59be
Michał Rybak <kontakt@mrybak.pl>
2013-04-17 20:17:38 +0200

    second test red

--- tests/pl/mrybak/utils/PositionalRepresentationSpec.groovy
+++ tests/pl/mrybak/utils/PositionalRepresentationSpec.groovy
@@ -1,15 +1,24 @@
 package pl.mrybak.utils
 
 class PositionalRepresentationTest extends spock.lang.Specification {
-    def "should not accept negative radix"() {
-        given:
-            PositionalRepresentation representation = new PositionalRepresentation(1000)
+    PositionalRepresentation representation = new PositionalRepresentation(1000)
 
+
+    def "should not accept negative radix"() {
         when:
             representation.getRepresentation(-2)
 
         then:
             thrown(IllegalArgumentException)
+
+    }
+
+    def "should not accept radix greater than 36"() {
+        when:
+        representation.getRepresentation(37)
+
+        then:
+        thrown(IllegalArgumentException)
 
     }
 }
